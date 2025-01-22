6f757a557e7b4cc822196b0f49800f40c185f504
Desai <samirgamzaev7@gmail.com>
2025-01-22 03:58:24 +0100

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -14,7 +14,7 @@ class AsciiMod(loader.Module):
         if len(event.text.split(" ", maxsplit=1)) > 1:
             text = event.text.split(" ", maxsplit=1)[1]
         else:
-            await event.edit("❌ Пожалуйста, укажите текст для генерации.")
+            await utils.answer(message, "❌ Пожалуйста, укажите текст для генерации.")
             return
         art = pyfiglet.figlet_format(text)
-        await event.edit(f"```\n⁠{art}\n```", parse_mode="markdown")
+        await utils.answer(message, f"```\n⁠{art}\n```", parse_mode="markdown")
