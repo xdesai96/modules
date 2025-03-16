6551dd0675f0ade6abf05033c1180e9903121678
Desai <samirgamzaev7@gmail.com>
2025-03-16 16:06:13 +0100

    new file:   SecretM.py

---
+++ SecretM.py
@@ -0,0 +1,62 @@
+# meta developer: @xdesai
+
+import logging
+from ..inline.types import InlineCall, InlineQuery
+from .. import loader
+
+@loader.tds
+class SecretMessageMod(loader.Module):
+    strings = {
+        "name": "SecretMessage",
+        "for_user_message": "🔐 Secret message for <b><a href='tg://user?id={id}'>{name}</a></b>",
+        "open": "👀 Open",
+        "no_user_or_message": "Specify the user and the message",
+        "secret_message": "Secret message",
+        "send_message": "Send secret message for {name}",
+        "help_message": "<b>Usage:</b>\n<code>@{bot} whisper (id/username) (text)</code>"
+    }
+
+    strings_ru = {
+        "name": "SecretMessage",
+        "for_user_message": "🔐 Секретное сообщение для <b><a href='tg://user?id={id}'>{name}</a></b>",
+        "open": "👀 Открыть",
+        "no_user_or_message": "Укажите пользователя и сообщение",
+        "secret_message": "Секретное сообщение",
+        "send_message": "Отправить секретное сообщение для {name}",
+        "help_message": "<b>Использование:</b>\n<code>@{bot} whisper (id/username) (текст)</code>"
+    }
+
+    @loader.inline_handler(ru_doc="Секретное сообщение для пользователя")
+    async def whisper(self, query: InlineQuery):
+        """Secret message for a user"""
+        if len(query.args.split()) > 1:
+            try:
+                if query.args.split()[0].isdigit():
+                    for_user = await self._client.get_entity(int(query.args.split()[0]))
+                else:
+                    for_user = await self._client.get_entity(query.args.split()[0])
+                text = " ".join(query.args.split()[1:])
+            except Exception as e:
+                logging.error(f"{e}")
+        else:
+            return {
+                "title": self.strings("secret_message"),
+                "description": self.strings("no_user_or_message"),
+                "message": self.strings("help_message").format(bot=(await self.inline.bot.get_me()).username),
+                "thumb": "https://img.icons8.com/?size=100&id=T9nkeADgD3z6&format=png&color=000000",
+            }
+        return {
+            "title": self.strings("secret_message"),
+            "description": self.strings("send_message").format(name=for_user.first_name),
+            "message": self.strings("for_user_message").format(id=for_user.id, name=for_user.first_name),
+            "thumb": "https://img.icons8.com/?size=100&id=kDMAGBvpqAyW&format=png&color=000000",
+            "reply_markup": {
+                "text": self.strings("open"),
+                "callback": self._handler,
+                "args": (text, query),
+                "always_allow": [for_user]
+            },
+        }
+
+    async def _handler(self, call: InlineCall, text: str):
+        await call.answer(f"{text}", show_alert=True)
