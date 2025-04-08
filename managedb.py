cf4a3543c8ed2c1f0ebf8b5ffb37db5686e1abc0
desai <samirgamzaev7@gmail.com>
2025-04-08 12:45:14 +0200

    Update managedb.py

--- managedb.py
+++ managedb.py
@@ -6,14 +6,12 @@ class DBMod(loader.Module):
     """Module to check and clean the database\nBe careful while using this module!"""
     strings = {"name": "DBMod",
         "del_text": "<b>Choose a module to delete from the database</b>\n\n⚠ Be careful and do not delete the core modules",
-        "info_text": "<b>Which of the modules are you interested in?</b>",
         "deleted": "Key {key} deleted from Database",
         "not_found": "Key {key} not found in Database"
     }
 
     strings_ru = {
         "del_text": "<b>Выберите модуль для удаления из базы данных</b>\n\n⚠ Будьте осторожны и не удаляйте основные модули",
-        "info_text": "<b>Какой из модулей вас интересует?</b>",
         "deleted": "Ключ {key} удален из базы данных",
         "not_found": "Ключ {key} не найден в базе данных"
     }
@@ -23,7 +21,7 @@ class DBMod(loader.Module):
         if item[0] in self._db.keys():
             self._db.pop(f"{item[0]}")
             self._db.save()
-            await call.edit(self.strings("del_text"), reply_markup=self.generate_markup("del"))
+            await call.edit(self.strings("del_text"), reply_markup=self.generate_info_all_markup())
             await call.answer(self.strings("deleted").format(key=item[0]))
             return True
         await call.answer(self.strings("not_found").format(key=item[0]))
@@ -32,54 +30,55 @@ class DBMod(loader.Module):
     async def info_db(self, call, item):
         """Info about db of the module"""
         if item[0] in self._db.keys():
-            await call.edit(f"<pre><code class='language-INFO'>{item[0]}:\n{item[1]}</code></pre>", reply_markup=self.generate_markup("info"))
+            await call.edit(f"<pre><code class='language-INFO'>{item[0]}:\n{item[1]}</code></pre>", reply_markup=self.generate_delete_markup(item))
             return True
         await call.answer(self.strings("not_found").format(key=item[0]))
         return False
 
-    def generate_markup(self, action: str = "info"):
+    def generate_delete_markup(self, item):
+        """Generate markup for inline form"""
+        markup = [[]]
+        markup[-1].append(
+            {
+                'text': f'◀ Back',
+                'callback': self.main_menu,
+            }
+        )
+        markup[-1].append(
+            {
+                'text': f'❌ Delete',
+                'callback': self.delete_db,
+                'args': [item],
+            }
+        )
+        return markup
+
+    async def main_menu(self, message):
+        await utils.answer(
+            message,
+            self.strings("del_text"),
+            reply_markup=self.generate_info_all_markup()
+        )
+
+    def generate_info_all_markup(self):
         """Generate markup for inline form"""
         markup = [[]]
         for item in self._db.items():
             if item not in markup:
                 if len(markup[-1]) == 4:
                     markup.append([])
-                if action == "del":
-                    markup[-1].append(
-                        {
-                            'text': f'{item[0]}',
-                            'callback': self.delete_db,
-                            'args': [item],
-                        }
-                    )
-                elif action == "info":
-                    markup[-1].append(
-                        {
-                            'text': f'{item[0]}',
-                            'callback': self.info_db,
-                            'args': [item],
-                        }
-                    )
+                markup[-1].append(
+                    {
+                        'text': f'{item[0]}',
+                        'callback': self.info_db,
+                        'args': [item],
+                    }
+                )
         return markup
-
-    @loader.command(
-        ru_doc="Удалить модуль из базы данных",
-    )
-    async def cldbcmd(self, message):
-        """Clean db of the module"""
-        await utils.answer(
-            message,
-            self.strings("del_text"),
-            reply_markup=self.generate_markup("del")
-        )
 
     @loader.command(
         ru_doc="Посмотреть модуль в базе данных",
     )
-    async def infodbcmd(self, message):
+    async def mydbcmd(self, message):
         """Check the info of the modules"""
-        await utils.answer(
-            message,
-            self.strings("info_text"),
-            reply_markup=self.generate_markup("info")
-        )
+        await self.main_menu(message=message)
